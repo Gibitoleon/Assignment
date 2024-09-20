84c73e1b68e7214f31f70b128ab6a327b8476ffc
Gabu <gabrielleon9928@gmail.com>
2024-09-20 10:59:47 +0300

    class for creating registration form

---
+++ Classes/Registration.php
@@ -0,0 +1,73 @@
+<?php
+Class Registration{
+public function showform(){
+    ?>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" 
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="Styles/Registration.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="registration-form">
+        <h2>Registration Form</h2>
+        <form id="form" action="validation.php">
+         <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  name="email" placeholder="Enter email">
+    
+       </div>
+            <!-- Username Field -->
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" placeholder="Enter username"
+                name="Username" required>
+            </div>
+
+            <!-- First Name Field -->
+            <div class="form-group">
+                <label for="firstName">First Name</label>
+                <input type="text" class="form-control" id="firstName" placeholder="Enter first name"
+                name="Firstname" required>
+            </div>
+
+            <!-- Last Name Field -->
+            <div class="form-group">
+                <label for="lastName">Last Name</label>
+                <input type="text" class="form-control" id="lastName" placeholder="Enter last name"
+                name="lastname" required>
+            </div>
+
+            <!-- Password Field -->
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Enter password"
+                name="Password" required>
+            </div>
+            <P> have an account?<a href="login.php">LogIn</a></P>
+            <!-- Submit Button -->
+            <button type="submit" class="btn btn-primary btn-block">SignUp</button>
+        </form>
+    </div>
+</div>
+<script
+      src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"
+      integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    ></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
+    
+    <script src="Scripts/registration.js"></script>
+</body>
+</html>
+ <?php   
+}
+}
