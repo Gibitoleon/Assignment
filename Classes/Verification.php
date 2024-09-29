42610aa397c462d7374fc8db0a16077a4b3048b7
Gabu <gabrielleon9928@gmail.com>
2024-09-29 20:09:50 +0300

    class for verification page

--- Classes/Verification.php
+++ Classes/Verification.php
@@ -31,7 +31,7 @@ class Verification {
                     <h1 class="text-center mb-4">Verify Your Email</h1>
                     <div class="card-body">
                         <p class="text-center">Enter the verification code sent to your email.</p>
-                        <form method="POST">
+                        <form id='form' method="POST"action ="Validation/verification_validate.php">
                             <div class="form-group">
                                 <label for="verification_code">Verification Code:</label>
                                 <input type="text" class="form-control" name="verification_code" required>
@@ -45,6 +45,15 @@ class Verification {
                     </div>
                 </div>
             </div>
+            <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"
+      integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    ></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
+    
+    <script src="Scripts/verification.js"></script>
         </body>
         </html>
         <?php
