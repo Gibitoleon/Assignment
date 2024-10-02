74fba78690b6757ba61bf2be601ed448df9a5bbd
Gabu <gabrielleon9928@gmail.com>
2024-10-02 17:42:00 +0300

    added trim to the input field

--- loginvalidation.php
+++ loginvalidation.php
@@ -3,8 +3,10 @@ require_once "load.php";
 if($_SERVER["REQUEST_METHOD"]==="POST"){
     
    $email=filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-   $password=filter_input(INPUT_POST, 'Password', FILTER_SANITIZE_SPECIAL_CHARS);
+   $password=trim(filter_input(INPUT_POST, 'Password', FILTER_SANITIZE_SPECIAL_CHARS));
 
+   
+   $queryObj = new Query();
    $user =$queryObj->selectEmail($email);
    
    
@@ -12,20 +14,23 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
    if($user){
     
     $passwordverified = password_verify($password,$user['password']);
+  //  var_dump($hashedpassword, $user['password']);
     //if password is invalid display an error message
-    if(!$passwordverified){
-        $message = ['status' => 'error', 'msg' => 'Invalid password'];
-    }
-    else{
-      //establish a session for the authenticated user
+    if($passwordverified){
+      $message = ['status' => 'success', 'msg' => 'Login successful'];
     
       session_start();
      $_SESSION['id'] = $user['id'];  
      $_SESSION['email'] = $user['email'];  
+    }
+    else{
+      //establish a session for the authenticated user
+      //send a successful message on log in
+      $message = ['status' => 'error', 'msg' => 'Invalid password :'.$user['password']];
+    
     //var_dump($_SESSION['user_id'],$_SESSION['email']);
 
-     //send a successful message on log in
-     $message = ['status' => 'success', 'msg' => 'Login successful'];
+     
      
     
     }
