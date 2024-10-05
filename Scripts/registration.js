d5a5065e50ccd23f98aa9cce006152c5a36a4f84
Gabu <gabrielleon9928@gmail.com>
2024-10-05 20:47:37 +0300

    modified the script for registration

--- Scripts/registration.js
+++ Scripts/registration.js
@@ -1,7 +1,17 @@
 const form = document.getElementById('form');
-
-
+const emailerror = document.querySelector('.Emailerror');
+const passworderror =document.querySelector('.Passworderror');
+const usernameerror =document.querySelector('.Usernameerror');
+const firstnameerror =document.querySelector('.Firstnameerror');
+const lastnameerror =document.querySelector('.lastnameerror')
+// 
 form.addEventListener('submit',async(event)=>{
+     //clear input fields
+     emailerror.innerHTML='';
+   firstnameerror.innerHTML='';
+   lastnameerror.innerHTML='';
+   usernameerror.innerHTML='';
+   passworderror.innerHTML='';
     //prevent default submission
   event.preventDefault();
 
@@ -16,21 +26,56 @@ form.addEventListener('submit',async(event)=>{
   try {
    const {data} =  await axios.post(form.action,formdata)
    
+   
    console.log(data);
-   console.log('Data from server:', data.msg);
-   console.log('Status from server:', data.Status);
+   
    console.log(form.action);
-    if(data.Status==="failed"){
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: `${data.msg}`,
-        confirmButtonText: 'OK'
-    });
-
-    }
-    else if (data.Status==="pending"){
+   
   
+     
+     
+      
+     Object.keys(data).forEach((key)=>{
+     
+      if(data[key].Status==='failed'){
+      
+      if(key==='email'){
+       
+        emailerror.innerHTML= `${data[key].msg}`
+        
+      
+
+      
+      }
+      if(key ==='firstname'){
+      
+        firstnameerror.innerHTML =`${data[key].msg}`
+        
+      }
+      if(key ==='lastname'){
+       
+        lastnameerror.innerHTML =`${data[key].msg}`
+        
+      }
+      if(key ==='username'){
+       
+        usernameerror.innerHTML =`${data[key].msg}`
+        
+      }
+      if(key ==='password'){
+       
+        passworderror.innerHTML =`${data[key].msg}`
+        
+      }
+    }
+    
+     })
+     
+     
+
+    
+     
+   if(data.Status==="pending"){
     Swal.fire({
       title: '🔒 Verify Your Email!',
        text: `${data.msg}`,
@@ -38,15 +83,24 @@ form.addEventListener('submit',async(event)=>{
       timer: 2000
   }).then(()=>{
     window.location.href="Verification.php";
+  })}
+   if( data.Status ==='failedSend'){
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: `${data.message}`,
+      confirmButtonText: 'OK'
   });
-}
-    
+
+  }  ;
+
+  
     
   } catch (error) {
     Swal.fire({
       icon: 'error',
       title: 'Oops...',
-      text: `Could not execute: ${error.message}`,
+      text: `Oops the issue  is indicated : ${error.message}`,
       confirmButtonText: 'OK'
   });
   } 
